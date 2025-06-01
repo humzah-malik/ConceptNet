9503fe079ec023a99cd57fcfe878f8cbb7534c84
humzah malik <humzahzahid23@gmail.com>
2025-06-01 13:54:49 -0400

    added speed insight s and web analytics from vercel

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
 import Landing from './pages/Landing.jsx';
 import NewMap from './pages/NewMap.jsx';
 import Gallery from './pages/Gallery.jsx';
 import MapView from './pages/MapView.jsx';
 import ShareView from './pages/ShareView.jsx';
-
 import './index.css';
+import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from '@vercel/analytics/react';
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Analytics />
+    <SpeedInsights />
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
